63fed164863ed015ec707234395bd6ff883b4720
Justin Love <hg@JustinLove.name>
2009-09-13 21:44:36 -0500

    update cgd

--- script/cgd/html.js
+++ script/cgd/html.js
@@ -34,34 +34,53 @@ Of course normally, it's only used to build fragments.
       });
       return text;
     } else {
-      return enclose(tag, nonArray(structure));
+      return enclose.apply(this, nonArray(structure, tag));
     }
   };
   CGD.HTML.from = from;
   
   // The regular value decode.
-  function nonArray(structure) {
+  function nonArray(structure, tag) {
     if (structure == null) {
-      return "";
+      return ["", tag];
     }
 
     var text = "";
     switch(typeof(structure)) {
       case 'object':
-        CGD.OBJECT.forEach(structure, function(x, tag) {
-          text += from(x, tag);
+        if (structure instanceof CGD.HTML.NoEscape) {
+          return [structure.s, tag];
+        }
+        CGD.OBJECT.forEach(structure, function(x, key) {
+          if (key.substr(0,1) == '_') {
+            tag += ' ' + key.substr(1) + '="' + x + '"';
+            if (key == '_id' && 
+                (tag.substr(0,5) == 'input' || tag.substr(0,6) == 'select')) {
+              tag += ' name="' + x + '"';
+            }
+          } else {
+            text += from(x, key);
+          }
         });
-        return text;
+        return [text, tag];
       case 'number':
-        return structure.toFixed(2);
+        return [structure.toFixed(2), tag];
       default:
-        return CGD.HTML.escape(structure);
+        return [CGD.HTML.escape(structure), tag];
     }
   };
   
-  function enclose(tag, text) {
+  function enclose(text, tag) {
     if (tag) {
-      return "<" + tag + ">" + text + "</" + tag + ">";
+      if (tag.substr(0,5) == 'input' || tag.substr(0,6) == 'select') {
+        tag = tag.replace(/^([\w\.]+)#(\w+)/, '$1 id="$2" name="$2"');
+      } else {
+        tag = tag.replace(/^([\w\.]+)#(\w+)/, '$1 id="$2"');
+      }
+      tag = tag.replace(/^(\w+)\.([\w\.]+)/, function(m, name, classes) {
+        return name + ' class="' + classes.replace('.', ' ') + '"';
+      });
+      return "<" + tag + ">" + text + "</" + tag.split(" ")[0] + ">";
     } else {
       return text;
     }
@@ -69,13 +88,56 @@ Of course normally, it's only used to build fragments.
 }());
 
 CGD.HTML.escape = function(s) {
-  return s.toString().replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  return s.toString().
+    replace(/&/g, '&amp;').
+    replace(/</g, '&lt;').
+    replace(/>/g, '&gt;').
+    replace(/\n/g, '<br/>');
 };
 
+CGD.HTML.NoEscape = function(s) {
+  if (this == window || this == CGD.HTML) {
+    return new arguments.callee(s);
+  } else {
+    this.s = s;
+    return this;
+  }
+};
+
+// shorthand for computed style property
+CGD.HTML.prop = function(element, p) {
+  var style = document.defaultView.getComputedStyle(element, "");
+  if (style) {
+    return parseInt(style.getPropertyValue(p), 10);
+  } else {
+    return 0;
+  }
+};
+
+CGD.HTML.addClassToIds = function(ids, className) {
+  CGD.ARRAY.forEach(ids, function(id) {
+    var el = document.getElementById(id);
+    if (el) {
+      el.className += ' ' + className;
+    }
+  });
+};
 
 // Rest are utility procedures for various tags.
 
-CGD.HTML.select = CGD.HTML.select || {};
+CGD.HTML.select = function(id) {
+  return CGD.HTML.from({
+    'div.select.control': {
+      _id: id + "_control",
+      'span.title.label': CGD.STRING.capital(id),
+      'select.popup': {
+        _id: id,
+        'option value="placeholder"': "Placeholder"
+      }
+    }
+  });
+};
+
 CGD.HTML.select.populate = function(selectId, arrayWithNames, initial)
 {
   var select = document.getElementById (selectId);
@@ -89,9 +151,10 @@ CGD.HTML.select.populate = function(selectId, arrayWithNames, initial)
 
   CGD.ARRAY.forEach(arrayWithNames, function(a, i) {
     var element = document.createElement('option');
-    element.text = a.name;
+    var name = a.name || a;
+    element.text = name;
     select.appendChild(element);
-    if (a.name === initial) {
+    if (name === initial) {
       select.selectedIndex = i;
     }
   });
